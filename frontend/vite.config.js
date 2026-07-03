848b9597c4883e163feb2b8ead702ceaa06f633b
raviraj-547 <ravirajsamota17@gmail.com>
2026-07-04 00:16:26 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import tailwindcss from "tailwindcss"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
 })
