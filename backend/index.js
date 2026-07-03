de2d156e3080b7b5580b3321a683e8e69cdc1e1e
Raviraj <ravirajsamota17@gmail.com>
2026-07-04 03:11:57 +0530

    Update API routes to remove full URL paths

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ const PORT = process.env.PORT || 3000;
 
 
 // api's
-app.use("https://job-portal-gks0.onrender.com/api/v1/user", userRoute);
-app.use("https://job-portal-gks0.onrender.com/api/v1/company", companyRoute);
-app.use("https://job-portal-gks0.onrender.com/api/v1/job", jobRoute);
-app.use("https://job-portal-gks0.onrender.com/api/v1/application", applicationRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 
 
